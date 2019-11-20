36ef9ec1585fef1e6f396986f94e3274a1033d85
Abi Vickery <vickeryabi@gmail.com>
2019-11-20 15:26:10 -0600

    insert sorter

--- js/app.js
+++ js/app.js
@@ -1,15 +1,34 @@
 // Bubble sorter
+function bubble(nums) {
 let nums = [10, 5, 3, 8, 2, 6, 4, 7, 9, 1];
+// let swapped = false;
 do {
-    let swapped = false;
+    var swapped = false;
     for (let i = 0; i < nums.length; i++) {
         // do stuff
         if (nums[i + 1] < nums[i]) { // 5 < 10
-            let temp = num[i];
-            num[i] = nums[i + 1];
+            let temp = nums[i];
+            nums[i] = nums[i + 1];
             nums[i + 1] = temp;
             swapped = true;
         }
     }
 } while (swapped);
 console.log(nums);
+}
+bubble(nums);
+
+// Insert sorter
+
+function insertSort(nums) {
+    for (let i = 0; i < Array.length; i++) {
+        for (let j = 0; j < Array.length; j++) {
+            if (nums[i] < nums[j]) {
+                const spliced = nums.splice(i, 1);
+                nums.splice(j, 0, spliced[0]);
+            }
+        }
+    }
+    console.log(nums);
+}
+insertSort(nums);
