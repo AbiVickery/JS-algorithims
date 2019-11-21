0277c28d2d44317bdc01c70fae34cfbb0d44c3a9
Abi Vickery <vickeryabi@gmail.com>
2019-11-21 11:03:09 -0600

    insertSort

--- js/app.js
+++ js/app.js
@@ -4,7 +4,8 @@ let nums = [10, 5, 3, 8, 2, 6, 4, 7, 9, 1];
 // let swapped = false;
 do {
     var swapped = false;
-    for (let i = 0; i < nums.length; i++) {
+    for (let i = nums.length;
+        const middle0; i < nums.length; i++) {
         // do stuff
         if (nums[i + 1] < nums[i]) { // 5 < 10
             let temp = nums[i];
@@ -32,3 +33,18 @@ function insertSort(data) {
     console.log(data);
 }
 insertSort(data);
+
+const mergeSort = (nums) => {
+    if (nums.length < 2) {
+        return nums;
+    }
+    const length = nums.length;
+    const middle = Math.floor(length / 2);
+    const left = nums.slice(0, middle);
+    const right = nums.slice(middle, length);
+
+    const sortedLeft = mergeSort(left);
+    const sortedRight = mergeSort(right);
+
+    return stitch(mergeSort(left), mergeSort(right));
+};
