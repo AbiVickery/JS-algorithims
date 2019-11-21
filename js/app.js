42ccb08a79aff06df5c7272644bc4a61999d5ae9
Abi Vickery <vickeryabi@gmail.com>
2019-11-21 15:29:48 -0600

    array list

--- js/app.js
+++ js/app.js
@@ -193,3 +193,135 @@ function quickSort(data) {
 }
 let quickSortResults = quickSort(nums);
 console.log('quickSort Results: ', quickSortResults);
+
+class LinkedList {
+    constructor() {
+      this.tail = this.head = null;
+      this.length = 0;
+    }
+    push(value) {
+      const node = new Node(value);
+      this.length++;
+      if (!this.head) {
+       this.head = node;
+      }
+      else {
+        this.tail.next = node;
+      }
+      this.tail = node;
+    }
+    pop() {
+      if (!this.head) return null;
+      if (this.head === this.tail) {
+        const node = this.head;
+        this.head = this.tail = null;
+        return node.value;
+      }
+      const penultimate = this._find(null, (value, nodeValue, i, current) => current.next === this.tail );
+      const ans = penultimate.next.value;
+      penultimate.next = null;
+      this.tail = penultimate;
+      this.length--;
+      return ans;
+    }
+    _find(value, test=this.test) {
+      let current = this.head;
+      let i = 0;
+      while(current) {
+        if (test(value, current.value, i, current)) {
+          return current;
+        }
+        current = current.next;
+        i++;
+      }
+      return null;
+    }
+    get(index) {
+      const node = this._find(index, this.testIndex);
+      if (!node) return null;
+      return node.value;
+    }
+    delete(index) {
+      if (index === 0) {
+        const head = this.head;
+        if (head) {
+          this.head = head.next;
+        }
+        else {
+          this.head = null;
+        }
+        this.length--;
+        return head.value;
+      }
+      
+      const node = this._find(index-1, this.testIndex);
+      const excise = node.next;
+      if (!excise) return null;    
+      node.next = excise.next;    
+      if (!node.next.next) this.tail = node.next;
+      this.length--;
+      return excise.value;
+    }
+    test(search, nodeValue) {
+      return search === nodeValue;
+    }
+    testIndex(search, __, i) {
+      return search === i;
+    }
+    serialize() {
+      const ans = [];
+      let current = this.head;
+      if (!current) return ans;
+      while (current) {
+        ans.push(current.value);
+        current = current.next;
+      }
+      return ans;
+    }
+  }
+  
+class ArrayList {
+    constructor() {
+        this.length = 0;
+        this.data = {};
+    }
+    push(value) {
+        this.data[this.length] = value;
+        this.length++;
+    }
+    pop(value) {
+        return this.pop(this.length - 1);
+    }
+    get(index) {
+        return this.data[index];
+    }
+    delete(index) {
+        const ans = this.data[index];
+        this._collapseTo(index);
+        return ans;
+    }
+    _collapseTo(index) {
+        for (let i = index; i < Array.length; i++) {
+            this.data[i] = this.data[i + 1];
+        }
+        delete this.data[this.length - 1];
+        this.length--;
+    }
+    print () {
+        donsole.log(this.data);
+    }
+}  
+
+let arrayList = new ArrayList();
+arrayList.push(2);
+arrayList.push(5);
+arrayList.push(7);
+
+arrayList.print();
+
+let temp = arrayList.pop();
+console.log(temp);
+arrayList.print();
+arrayList.push(8);
+arrayList.push(12);
+arrayList.push(13);
